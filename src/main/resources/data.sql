1fda4f39ca2a2c173a6d0771ea029e2b7129c80d
Faizan2901 <mo.faizansopariwala2901@gmail.com>
2024-07-10 15:59:23 +0530

    Implemented currency-exchnage-service

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO currency_exchange(conversion_multiple,id,currency_from,currency_to,environment) VALUES (85,10001,'USD','INR','');
+INSERT INTO currency_exchange(conversion_multiple,id,currency_from,currency_to,environment) VALUES (22,10002,'SAR','INR','');
+INSERT INTO currency_exchange(conversion_multiple,id,currency_from,currency_to,environment) VALUES (100,10003,'EUR','INR','');
+INSERT INTO currency_exchange(conversion_multiple,id,currency_from,currency_to,environment) VALUES (271,10004,'DIN','INR','');
